6da356a0a6d69dc9877f36e07e6d7b5ce4e0980d
JD Fuller <jdfuller@live.com>
2020-07-26 21:45:51 -0600

    spacing-changes
    changes to spacing have been made

--- app/Controllers/SpellController.js
+++ app/Controllers/SpellController.js
@@ -1,41 +1,57 @@
 import store from "../store.js";
 import service from "../Services/SpellService.js";
 
-
-
 //Private
-function _drawApiSpells() { 
-  let template = ''
+function _drawApiSpells() {
+  let template = "";
   let spells = store.State.apiSpells;
-  spells.forEach(s => template += `<li onclick="app.spellController.selectSpellAsync('${s.id}')">${s.name}</li>`)
-  document.getElementById("spells").innerHTML = template
-  console.log("from the _drawApiSpells", spells);
+  spells.forEach(
+    s =>
+      (template += `<li onclick="app.spellController.selectSpellAsync('${s.id}')">${s.name}</li>`)
+  );
+  document.getElementById("spells").innerHTML = template;
 }
 
-function _drawActiveSpell(){ 
+function _drawActiveSpell() {
   let spell = store.State.activeSpell;
-    document.getElementById("active").innerHTML = spell.Template
-  }
+  document.getElementById("active").innerHTML = spell.Template;
+}
+
+function _drawMySpells() {
+  let template = "";
+  let spells = store.State.mySpells;
+  spells.forEach(
+    s =>
+      (template += `<li onclick="app.spellcontroller.selectMySpellAsync('${s.id}')">${s.name}</li>`)
+  );
+  document.getElementById("mySpells").innerHTML = template;
+}
 
 //Public
 export default class SpellController {
   constructor() {
+    console.log("Hello from the controller");
     store.subscribe("apiSpells", _drawApiSpells);
-    store.subscribe("activeSpell", _drawActiveSpell)
-    service._getApiSpellsAsync(); //run the get spells async 
-    service.selectSpellAsync();
-    _drawActiveSpell();
-    _drawApiSpells(); // draw spells onto the sreen
+    store.subscribe("activeSpell", _drawActiveSpell);
+    store.subscribe("mySpells", _drawMySpells);
+    service._getApiSpellsAsync(); //run the get spells async
   }
 
-  async selectSpellAsync(id){
-    debugger
+  async selectSpellAsync(id) {
+    debugger;
     try {
-      await service.selectSpellAsync(id)
+      await service.selectSpellAsync(id);
     } catch (error) {
-      debugger
-      console.error(error)
+      debugger;
+      console.error(error);
     }
   }
-  
+
+  async addSpellAsync() {
+    try {
+      await service.addSpellAsync();
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
--- app/Services/SpellService.js
+++ app/Services/SpellService.js
@@ -1,56 +1,55 @@
 import store from "../store.js";
-import Spell from "../Models/Spell.js"
+import Spell from "../Models/Spell.js";
 
-
-let _spellsApi = axios.create ({
-  baseURL: 'http://bcw-sandbox.herokuapp.com/api/spells'
-})
+let _spellsApi = axios.create({
+  baseURL: "http://bcw-sandbox.herokuapp.com/api/spells"
+});
 
 let _sandbox = axios.create({
-  baseURL: 'http://bcw-sandbox.herokuapp.com/api/jdfuller/spells'
-})
+  baseURL: "http://bcw-sandbox.herokuapp.com/api/jdfuller/spells"
+});
 class SpellsService {
-
-  constructor(){
-console.log("hello from the spell service")
-this.getMySpellsAsync();
+  constructor() {
+    console.log("hello from the spell service");
+    this.getMySpellsAsync();
   }
 
-  async _getApiSpellsAsync(){
+  async _getApiSpellsAsync() {
     //Retrieve spell from API
-    let res = await _spellsApi.get('') 
+    let res = await _spellsApi.get("");
     //set 'res' to the data from the API
-      store.commit('apiSpells', res.data) 
-      //sending data to the store..should be in the store now
-      console.log("from the getApiSpells", res.data) 
-      //print it out to test it
-      console.log("just the res from getApiSpells", res)
-    }
+    store.commit("apiSpells", res.data);
+    //sending data to the store..should be in the store now
+    console.log("from the getApiSpells", res.data);
+    //print it out to test it
+    console.log("just the res from getApiSpells", res);
+  }
 
-  async selectSpellAsync(id){ //Grab an individual spell from the list
-    let res = await _spellsApi.get(id)
-    debugger
-    console.log("from the selectspellasync", res)
-    let theActiveSpell = new Spell(res.data);//creating new instance of Spell, calling it activeSpell, filled with data
-    store.commit("activeSpell", theActiveSpell);//Commit new instance to store
-    console.log("from store.activeSpell", store.State.activeSpell)
-
+  async selectSpellAsync(id) {
+    //Grab an individual spell from the list
+    let res = await _spellsApi.get(id);
+    debugger;
+    console.log("from the selectspellasync", res);
+    let theActiveSpell = new Spell(res.data); //creating new instance of Spell, calling it activeSpell, filled with data
+    store.commit("activeSpell", theActiveSpell); //Commit new instance to store
+    console.log("from store.activeSpell", store.State.activeSpell);
   }
 
   async addSpellAsync() {
-    let spell = store.State.activeSpell
-    let res = await _sandbox.post("", spell)
-    console.log("addSpellAsync", spell)
+    let spell = store.State.activeSpell;
+    let res = await _sandbox.post("", spell);
+    console.log("addSpellAsync", spell);
     this.getMySpellsAsync();
-
   }
 
-  async getMySpellsAsync(){
-    let res = await _sandbox.get()
-    store.commit("mySpells", res.data.data.map(s => new Spell(s)))
-    console.log("my spells", store.State.mySpells)
+  async getMySpellsAsync() {
+    let res = await _sandbox.get();
+    store.commit(
+      "mySpells",
+      res.data.data.map(s => new Spell(s))
+    );
+    console.log("my spells", store.State.mySpells);
   }
-
 }
 
 const service = new SpellsService();
